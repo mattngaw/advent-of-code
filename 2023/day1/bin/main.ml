dec173313e4eded8b3001bf84163754b2df05692
mattngaw <me@mattngaw.com>
2024-01-04 11:01:13 -0500

    Add comments to first day1 part1 solution

--- 2023/day1/bin/main.ml
+++ 2023/day1/bin/main.ml
@@ -1,15 +1,18 @@
 let file : string = "input.txt"
 
-(* explode :  *)
+(* explode : string -> char list *)
 let explode (s : string) : char list = 
     List.init (String.length s) (String.get s)
 
+(* is_digit : char -> bool *)
 let is_digit (c: char) : bool = 
     ('0' <= c) && (c <= '9')
 
+(* digit_to_int : char -> int *)
 let digit_to_int (c : char) : int = 
     Char.code c - Char.code '0'
 
+(* first_last : char list -> char * char *)
 let rec first_last (cs : char list) : char * char = 
     match cs with
     | [] -> failwith "Not enough elements"
@@ -20,11 +23,11 @@ let rec first_last (cs : char list) : char * char =
 let part1 () : unit =
     let ic = open_in file in
     let lines = In_channel.input_lines ic in
-    let values = List.map (fun line ->
-        let chars = explode line in
-        let digits = List.filter is_digit chars in
-        let (c1,c2) = first_last digits in
-        (digit_to_int c1 * 10) + (digit_to_int c2)
+    let values = List.map (fun line ->              (* For each line... *)
+        let chars = explode line in                 (* Explodes the line into chars... *)
+        let digits = List.filter is_digit chars in  (* Keeps only the digits *)
+        let (c1,c2) = first_last digits in          (* Gets the first and last digits *)
+        (digit_to_int c1 * 10) + (digit_to_int c2)  (* Converts the chars into numbers *)
     ) lines in 
     let sum = List.fold_right ( + ) values 0 in
     Printf.printf "%d\n%!" sum;
