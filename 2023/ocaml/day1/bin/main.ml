b19a4a71cde6d08d6e57ef34a2e9ea2f8b957f01
mattngaw <me@mattngaw.com>
2024-01-10 20:04:56 -0500

    Finished 2023 day 1 part 2 in OCaml

--- 2023/ocaml/day1/bin/main.ml
+++ 2023/ocaml/day1/bin/main.ml
@@ -1,37 +1,75 @@
 let file : string = "input.txt"
 
-let digit_to_int (c : char) : int = 
+let (>>) f g = fun x -> g(f(x))
+
+let explode (s : string) : char list = 
+    List.init (String.length s) (String.get s)
+
+let is_digit (c : char) : bool =
+    '0' <= c && c <= '9'
+
+let digit_to_int (c : char) : int =
     Char.code c - Char.code '0'
 
-let rec first_last (cs : char list) : char * char = 
-    match cs with
+let rec first_last (xs : 'a list) : 'a * 'a = 
+    match xs with
     | [] -> failwith "Not enough elements"
-    | [c] -> (c,c)
-    | [c1;c2] -> (c1,c2) 
-    | c::_::cs -> first_last (c::cs)
+    | [x] -> (x,x)
+    | [x1;x2] -> (x1,x2) 
+    | x::_::xs -> first_last (x::xs)
 
-let part1 () : unit =
-    let ic = open_in file in
-    let lines = In_channel.input_lines ic in
-    let values = List.map (fun line ->
-        let r = Str.regexp {|\d|} in
-        let rec get_all_matches i = match Str.search_forward r line i with
-            | i -> (Str.matched_string line) :: (get_all_matches (i+1))
-            | exception Not_found -> []
-        in
-        let matches = get_all_matches 0 in
-        let chars = List.map (fun str -> String.get str 0) matches in
-        let (first, last) = first_last chars in
-        let (first, last) = (digit_to_int first, digit_to_int last) in
-        first * 10 + last
-    ) lines in 
-    let sum = List.fold_right ( + ) values 0 in
-    Printf.printf "%d\n%!" sum;
-    close_in ic
+let part1 : string list -> int =
+    List.map (
+        explode >>
+        List.filter is_digit >>
+        List.map digit_to_int >>
+        first_last >>
+        (fun (x1, x2) -> x1 * 10 + x2)
+    ) >>
+    List.fold_left ( + ) 0
 
-let part2 () : unit =
-    ()
+let string_to_digits =
+    [ ("one", 1)
+    ; ("two", 2)
+    ; ("three", 3)
+    ; ("four", 4)
+    ; ("five", 5)
+    ; ("six", 6)
+    ; ("seven", 7)
+    ; ("eight", 8)
+    ; ("nine", 9)
+    ]
+
+let rec find_digits (s : string) : int list =
+    if s = String.empty then [] else
+    let next_len = String.length s - 1 in
+    let substring = String.sub s 1 next_len in
+    let next_digits = find_digits substring in
+    match
+        List.find_opt (fun (word, _) ->
+            String.starts_with ~prefix:word s
+        ) string_to_digits
+    with
+    | None -> 
+        let c = String.get s 0 in
+        let digit = digit_to_int c in
+        if is_digit c then digit :: next_digits
+        else next_digits
+    | Some((_, digit)) ->
+        digit :: next_digits
+    
+let part2 : string list -> int =
+    List.map (
+        find_digits >>
+        first_last >>
+        (fun (x1, x2) -> x1 * 10 + x2)
+    ) >>
+    List.fold_left ( + ) 0
 
 let () =
-    part1();
-    part2();
+    let ic = open_in file in
+    let lines = In_channel.input_lines ic in
+    let () = print_int (part1 lines) in
+    let () = print_newline () in
+    let () = print_int (part2 lines) in
+    close_in ic
