1fefb4891aa7f4598218cc625933dea73250b191
Matt Ngaw <mattngaw2002@gmail.com>
2025-12-01 13:36:32 -0500

    Initial commit; complete 2025 day 1

---
+++ 2025/day01/bin/main.ml
@@ -0,0 +1,9 @@
+open Day01
+
+(* Part 1 *)
+let () = Printf.printf "P1 example: %d\n" (Part1.solve "inputs/example.txt")
+let () = Printf.printf "P1 main: %d\n" (Part1.solve "inputs/main.txt")
+
+(* Part 2 *)
+let () = Printf.printf "P2 example: %d\n" (Part2.solve "inputs/example.txt")
+let () = Printf.printf "P2 main: %d\n" (Part2.solve "inputs/main.txt")
---
+++ 2025/day01/lib/part2.ml
@@ -0,0 +1,37 @@
+let solve (filename : string) : int =
+  let read (filename : string) : string list = 
+    In_channel.with_open_text filename In_channel.input_lines
+  in
+
+  let preprocess : (string list -> int list) = 
+    let transform (s : string) : int =
+      let len = String.length s in
+      let direction = String.get s 0 in
+      let magnitude = String.sub s 1 (len-1) |> Stdlib.int_of_string in
+      if direction = 'L' then -magnitude else magnitude
+    in
+
+    List.map transform
+  in
+
+  let modulo x y =
+    let result = x mod y in
+    if result >= 0 then result else result + y
+  in
+
+  let rec step (zeros : int) (dial : int) : (int list -> int) = function
+    | [] -> zeros
+    | x::xs -> 
+        let dial' = modulo (dial + x) 100 in
+        let zeros' = 
+          if dial = 0 then zeros + Int.abs(x) / 100
+          else if dial + x <= 0 then zeros + 1 - (dial + x) / 100
+          else zeros + (dial + x) / 100
+        in
+        step zeros' dial' xs
+  in
+
+  let base : (int list -> int) = step 0 50 in
+
+  filename |> read |> preprocess |> base
+
